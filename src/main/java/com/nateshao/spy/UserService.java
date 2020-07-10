02d13ff21034920b12a0c9be79785cb7f4676cc1
tongjie.shao@aax.com <tongjie.shao@aax.com>
2020-07-10 10:26:37 +0800

    spy

---
+++ src/main/java/com/nateshao/spy/UserService.java
@@ -0,0 +1,16 @@
+package com.nateshao.spy;
+
+/**
+ * @Author TongJie Shao
+ * Created by @Author  on 2020/7/10 10:13
+ */
+public class UserService {
+
+    public void fool(){
+        log();
+    }
+
+    public void log(){
+        System.out.println("i am console log");
+    }
+}
---
+++ src/test/java/com/nateshao/spy/UserServiceTest.java
@@ -0,0 +1,16 @@
+package com.nateshao.spy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author TongJie Shao
+ * Created by @Author  on 2020/7/10 10:14
+ */
+public class UserServiceTest {
+
+    @Test
+    public void fool() {
+    }
+}
