ce875a70ecd18b8d9e3e9aa50b2ff06b0ba3cd64
nateshao <shaotongjie@gmail.com>
2020-11-26 15:13:34 +0800

    添加文档

---
+++ src/main/java/com/aax/learn/dao/StudentEntityDao.java
@@ -0,0 +1,13 @@
+package com.aax.learn.dao;
+
+/**
+ * @date Created by 邵桐杰 on 2020/11/26 15:13
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description：
+ */
+public interface StudentEntityDao {
+}
