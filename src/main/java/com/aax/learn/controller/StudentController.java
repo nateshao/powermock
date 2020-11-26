0515ed68dd0f9e533f8db09ec90b3c7737cee990
nateshao <shaotongjie@gmail.com>
2020-11-26 15:14:17 +0800

    添加文档

--- src/main/java/com/aax/learn/controller/StudentController.java
+++ src/main/java/com/aax/learn/controller/StudentController.java
@@ -1,5 +1,10 @@
 package com.aax.learn.controller;
 
+import com.aax.learn.common.RestfulPath;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
 /**
  * @date Created by 邵桐杰 on 2020/11/26 15:12
  * @微信公众号 千羽的编程时光
@@ -9,5 +14,31 @@ package com.aax.learn.controller;
  * @Gitee https://gitee.com/nateshao
  * Description：
  */
+@Slf4j
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping(value = RestfulPath.getStudents)
+    public List<StudentEntity> getStudents(StudentEntity req) {
+        return studentService.findList(req);
+    }
+
+    @PostMapping(value = RestfulPath.postStudent, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public StudentEntity create(@RequestBody StudentEntity req) {
+        return studentService.insert(req);
+    }
+
+    @PutMapping(value = RestfulPath.putStudent, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public StudentEntity update(@RequestBody StudentEntity req) {
+        return studentService.update(req);
+    }
+
+    @DeleteMapping(value = RestfulPath.deleteStudent)
+    public StudentEntity delete(@PathVariable("id") long id) {
+        return studentService.delete(id);
+    }
 }
