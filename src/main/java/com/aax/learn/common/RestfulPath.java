8f78da956ad4880a2d39140526dd050b48c88592
nateshao <shaotongjie@gmail.com>
2020-11-26 15:12:42 +0800

    添加文档

---
+++ src/main/java/com/aax/learn/common/RestfulPath.java
@@ -0,0 +1,20 @@
+package com.aax.learn.common;
+
+/**
+ * @date Created by 邵桐杰 on 2020/11/26 15:11
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description：
+ */
+public class RestfulPath {
+    /**
+     * ----- product ----
+     **/
+    public final static String getStudents = "/v1/learn/students";
+    public final static String putStudent = "/v1/learn/student";
+    public final static String postStudent = "/v1/learn/student";
+    public final static String deleteStudent = "/v1/learn/student/{id}";
+}
